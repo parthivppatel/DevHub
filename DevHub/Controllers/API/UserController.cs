64e12aa10cc1b8cbb67dad38a06eb2589eec918d
parthiv <parthivp800@gmail.com>
2023-10-12 00:31:33 +0530

    Added Delete Account API for users

--- DevHub/Controllers/API/UserController.cs
+++ DevHub/Controllers/API/UserController.cs
@@ -34,7 +34,40 @@ namespace DevHub.Controllers.API
                 }
             }
 
-            return BadRequest("User ID not found.");
+            return BadRequest("User not found.");
         }
+
+        [HttpDelete]
+        [Authorize]
+        public IHttpActionResult DeleteAccount()
+        {
+            var userManager = HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            var identity = HttpContext.Current.User.Identity as ClaimsIdentity;
+            if (identity != null)
+            {
+                var userIdClaim = identity.FindFirst(ClaimTypes.NameIdentifier);
+                if (userIdClaim != null)
+                {
+                    var userId = userIdClaim.Value;
+                    var user = userManager.FindById(userId);
+                    if (user != null)
+                    {
+                        var result = userManager.Delete(user);
+                        if (result.Succeeded)
+                        {
+                            return Ok("User deleted successfully.");
+                        }
+                        else
+                        {
+                            return BadRequest("Error deleting user.");
+                        }
+                    }
+                }
+             
+            }
+            
+            return BadRequest("User not found.");
+        }
+
     }
 }
